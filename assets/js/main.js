2ebc15dfc21d0613dcafeb2f80a1ca7e0de27a2b
Shiva Kakarla <shiva.kakarla512@gmail.com>
2021-07-01 12:17:41 +0530

    final touches

--- assets/js/main.js
+++ assets/js/main.js
@@ -253,19 +253,19 @@ TweenMax.to('.main', 3, {
 });
 
 // NAV
-// gsap.from('.nav__logo', {opacity: 0, duration: 1, delay:1, y: 20})
-// gsap.from('.nav__item, .nav__moon', {opacity: 0, duration: 1, delay: 1, y: 20, stagger: 0.2})
+gsap.from('.nav__logo', {opacity: 0, duration: 1, delay:1, y: 20})
+gsap.from('.nav__item, .nav__moon', {opacity: 0, duration: 1, delay: 1, y: 20, stagger: 0.2})
 
 // HOME
-// gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
-// gsap.from('.home__subtitle', {opacity: 0, duration: 1, delay:1.7, y: 30})
-// gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
-// gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
-// gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
+gsap.from('.home__subtitle', {opacity: 0, duration: 1, delay:1.7, y: 30})
+gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
+gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
+gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
 
-// gsap.from('.home__social-icon', {opacity: 0, duration: 1, delay:2, x: -30, stagger: 0.2})
-// gsap.from('.home__scroll', {opacity: 0, duration: 1, delay:2.2, y: -30})
-// gsap.from('.home__note', {opacity: 0, duration: 1, delay:2.4, y: -30})
+gsap.from('.home__social-icon', {opacity: 0, duration: 1, delay:2, x: -30, stagger: 0.2})
+gsap.from('.home__scroll', {opacity: 0, duration: 1, delay:2.2, y: -30})
+gsap.from('.home__note', {opacity: 0, duration: 1, delay:2.4, y: -30})
 
-// gsap.from('.section__title', {opacity: 0, duration: 1, delay:2.8, y: -30})
-// gsap.from('.section__subtitle', {opacity: 0, duration: 1, delay:3.0, y: -30})
+gsap.from('.section__title', {opacity: 0, duration: 1, delay:2.8, y: -30})
+gsap.from('.section__subtitle', {opacity: 0, duration: 1, delay:3.0, y: -30})
